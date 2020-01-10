f8a87cfe3e82995f68b9b98726a20cc0ad12d1b0
naveen-ku <naveen2951996@gmail.com>
2020-01-10 17:51:28 +0530

    Adding a user registration in backend

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 //DB Config
 const db = require("./config/keys").mongoURI;
@@ -13,18 +18,18 @@ const db = require("./config/keys").mongoURI;
 //Connect to MongoDB
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDB COnnected"))
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("hello world !!");
-}); 
+});
 
 //Use routes
 
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 3000;
 
