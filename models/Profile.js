33dbabc5d54c843c543bf0776eb2cdc7c1ff0b59
naveen-ku <naveen2951996@gmail.com>
2020-01-25 01:10:08 +0530

    Adding profile validations

--- models/Profile.js
+++ models/Profile.js
@@ -26,7 +26,7 @@ const ProfileSchema = new Schema({
     required: true
   },
   skills: {
-    type: String,
+    type: [String],
     required: true
   },
   bio: {
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+//Load validation
+const validateProfileInput = require("../../validation/profile");
+
 //Load Profile Model
 const Profile = require("../../models/Profile");
 //Load Use Profile
@@ -42,6 +45,14 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      //Return any errros with 400 status
+      return res.status(400).json(errors);
+    }
+
     //Get fields
     const profileFields = {};
     profileFields.user = req.body.id;
@@ -65,29 +76,29 @@ router.post(
     if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
     if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
 
-    Profile.findOne({ user: req.user.id }).then(profile => {
+    Profile.findOne({ user: req.user.id })
+    .populate('user',['name','avatar'])
+    .then(profile => {
       if (profile) {
         // Update
         Profile.findOneAndUpdate(
           { user: req.user.id },
           { $set: profileFields },
           { new: true }
-        )
-          .then(profile => res.json(profile));
+        ).then(profile => res.json(profile));
       } else {
         //Create
 
         //Check if handle exists
-        Profile.findOne({handle: profileFields.handle})
-          .then(profile => {
-            if(profile){
-              errors.handle = 'That handle already exists';
-              res.status(400).json(errors);
-            }
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = "That handle already exists";
+            res.status(400).json(errors);
+          }
 
-            //Save Profile
-            new Profile(profileFields).save().then(profile => res.json(profile));
-          });
+          //Save Profile
+          new Profile(profileFields).save().then(profile => res.json(profile));
+        });
       }
     });
   }
