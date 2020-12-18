a5c2e2886044ebedcd0a6116d1d03df5568a95b9
naveen-ku <naveen2951996@gmail.com>
2020-12-18 15:43:57 +0530

    adding spinner and private route

---
+++ client/src/components/common/Spinner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SpinnerGif from "./spinner.gif";
+function Spinner() {
+  return (
+    <div>
+      <img
+        src={SpinnerGif}
+        style={{ width: "200px", margin: "auto", display: "block" }}
+        alt="Loading..."
+      />
+    </div>
+  );
+}
+
+export default Spinner;
