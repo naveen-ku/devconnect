05df983212b8686303c883e6e4810de9cf6b38b9
naveen-ku <naveen2951996@gmail.com>
2020-12-22 10:09:49 +0530

    Adding routes for adding experience and education with functionality

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,5 +1,11 @@
 import axios from "axios";
-import { CLEAR_CURRENT_PROFILE, GET_ERRORS, GET_PROFILE, PROFILE_LOADING } from "./types";
+import {
+  CLEAR_CURRENT_PROFILE,
+  GET_ERRORS,
+  GET_PROFILE,
+  PROFILE_LOADING,
+  SET_CURRENT_USER,
+} from "./types";
 
 // Get current profile
 
@@ -29,10 +35,37 @@ export const createProfile = (profileData, history) => (dispatch) => {
     .catch((err) => {
       dispatch({
         type: GET_ERRORS,
-        payload:err.response.data
-      })
+        payload: err.response.data,
+      });
     });
 };
+
+// Add Experience
+export const addExperience = (expData, history) => (dispatch) => {
+  axios
+    .post("/api/profile/experience", expData)
+    .then((res) => history.push("/dashboard"))
+    .catch((err) => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      });
+    });
+};
+
+//Add Education
+export const addEducation = (eduData, history) => (dispatch) => {
+  axios
+    .post("/api/profile/education", eduData)
+    .then((res) => history.push("/dashboard"))
+    .catch((err) => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      });
+    });
+};
+
 // Profile loading
 export const setProfileLoading = () => {
   return {
@@ -45,4 +78,24 @@ export const clearCurrentProfile = () => {
   return {
     type: CLEAR_CURRENT_PROFILE,
   };
+};
+
+//Delete Account and Profile
+export const deleteAccount = () => (dispatch) => {
+  if (window.confirm("Are you sure? This action cannot be undone!")) {
+    axios
+      .delete("/api/profile")
+      .then((res) =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {},
+        })
+      )
+      .catch((err) =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data,
+        })
+      );
+  }
 };
